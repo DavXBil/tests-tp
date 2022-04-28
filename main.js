f80eeaaeacf2752b20058983d00526a924b4ee43
DavidB <david.b@hotmail.fr>
2022-04-28 16:12:03 +0200

    modif test

---
+++ main.js
@@ -0,0 +1,48 @@
+import faker from "@faker-js/faker";
+
+    const randomNumber = faker.datatype.number(10)
+    const tva = 0.2
+
+    
+    const createCart = () => {
+           let cart = []
+           for (let i = 0; i < randomNumber; i++) {
+                   cart.push({
+                           name: faker.commerce.product(),
+                           price: faker.commerce.price(5, 60),
+                           weight: faker.datatype.number(15)
+                   });
+           }
+
+           console.log(cart)
+        
+        return cart
+    }
+    
+    // console.log(createPanier([produit,produit2]))
+    
+    function getTotal(cart) {
+        let total = 0
+        cart.forEach(product => {
+                total += parseInt(product.price)  
+        });
+
+        console.log(total)
+
+        return total
+    }
+    
+    // console.log(getPrixTotalPanier());
+    
+    export const getTVA = (total ,tva) => {
+
+        const TvaAmount = (total * tva) / 100
+        const price = total + TvaAmount
+        return price
+    }
+
+const cart = createCart()
+
+export const total = getTotal(cart)
+
+console.log(getTVA(total, tva))
--- test/demo.test.js
+++ test/demo.test.js
@@ -1,11 +1,17 @@
 import * as should from 'should'
+import {createCart, getTVA, total} from "../main.js"
 var User = function(name){  
     this.name = name;
 };
-describe("Checking if the user is created correctly", function(){  
-    it("should create the user with the correct name", function(){
-        debugger
-        var tom = new User("tom");
-        tom.name.should.be.equal("tom");
+describe("Checking if cart generation runs without problems", () => {  
+    it("asking if cart is an object", function(){
+        
+        should(createCart()).be.instanceof(Object);
     });
+});
+
+describe("Check if TVA runs correctly", () => {
+    it("Asking if tva is a number", () => {
+        should(getTVA(total).be.a.Number())
+    })
 });
